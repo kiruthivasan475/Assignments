0e81d2739de825f80bf9b8a7297da32128a9b374
kiruthivasan475 <155840853+kiruthivasan475@users.noreply.github.com>
2024-01-06 23:03:10 +0530

    Add files via upload

---
+++ FindNumber.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		int num=45;
+		
+		if (num>0) {
+			
+			System.out.println("The number is positive :"+num);
+		} else {
+
+			System.out.println("The number is negative :"+num);
+		}
+
+	}
+
+}
